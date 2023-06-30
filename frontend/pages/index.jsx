4a46b9cbbef95a23c76cc78c0733c4cbd879e083
Remora Blackflag <104504762+remorablackflag@users.noreply.github.com>
2023-06-30 21:19:08 +0200

    formatting

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -2,11 +2,11 @@ import styles from "../styles/Home.module.css";
 import InstructionsComponent from "../components/InstructionsComponent";
 
 export default function Home() {
-  return (
-    <div>
-      <main className={styles.main}>
-        <InstructionsComponent></InstructionsComponent>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <main className={styles.main}>
+                <InstructionsComponent></InstructionsComponent>
+            </main>
+        </div>
+    );
 }
